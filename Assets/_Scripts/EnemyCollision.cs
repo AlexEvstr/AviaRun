f08054e10ef1cef33111cbe82918b394a8ac472b
Alexander Evstratov <alexdrugev@me.com>
2024-02-02 10:47:01 +0300

    Add explosion effect.

--- Assets/_Scripts/EnemyCollision.cs
+++ Assets/_Scripts/EnemyCollision.cs
@@ -7,5 +7,9 @@ public class EnemyCollision : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Game Over");
+        }
     }
 }
--- Assets/_Scripts/RocketCollision.cs
+++ Assets/_Scripts/RocketCollision.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class RocketCollision : MonoBehaviour
 {
+    [SerializeField] private GameObject _explosion;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag("Player"))
+        {
             Destroy(gameObject);
+            GameObject explosion = Instantiate(_explosion);
+            explosion.transform.position = new Vector3(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z);
+        }
     }
 }
